7626cf58f7d74216afc5069aae1cfafa3d72f778
James Mann <james.mann@bcs.org>
2016-12-16 13:17:21 +0000

    changes

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 var restify = require('restify');
 var builder = require('botbuilder');
 
+//=========================================================
 // Bot Setup
+//=========================================================
+
+// Setup Restify Server
 var server = restify.createServer();
-server.listen(3978, function () {
+server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
   
+// Create chat bot
 var connector = new builder.ChatConnector({
     appId: '80ed060b-2ea3-4db8-8c66-b091ee3fb987',
     appPassword: 'B02a0d3vUncniaJ9yfFynCU'
@@ -14,7 +19,12 @@ var connector = new builder.ChatConnector({
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
+//=========================================================
 // Bots Dialogs
+//=========================================================
+
 bot.dialog('/', function (session) {
-    session.send("Hello from Azure");
+    session.send("Hello World");
 });
+
+
